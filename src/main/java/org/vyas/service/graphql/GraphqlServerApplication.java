c9fbedbc1124f6174efcc7baec604ba4057db8d2
vyasmus <vyasmus@gmail.com>
2026-06-12 10:51:40 -0400

    Working Copy.

---
+++ src/main/java/org/vyas/service/graphql/GraphqlServerApplication.java
@@ -0,0 +1,8 @@
+package org.vyas.service.graphql;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQLService {
+    public static void main(String[] args) {}
+}
